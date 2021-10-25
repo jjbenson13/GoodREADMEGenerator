c8ba43dc6332a2e5fb260ee96cf2b799a27931d1
Jasmine Benson <jjortega2010@gmail.com>
2021-10-25 07:44:01 -0700

    Pusshing before questions

---
+++ index.js
@@ -0,0 +1,10 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const utill = require("utill");
+
+const generatorMarkdown = require("./utill/generatorMarkdown");
+
+//Question for users
+const questions = [{
+  t
+}]
